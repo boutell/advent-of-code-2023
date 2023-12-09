4c8bf6940935f4df44b7e0f07c04c9b77d9dc141
boutell <tommybgoode@gmail.com>
2023-12-09 09:46:53 -0500

    9a

---
+++ 9a.mjs
@@ -0,0 +1,39 @@
+import { readFileSync } from 'fs';
+
+const input = readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n').filter(line => line.length > 0);
+
+const sequences = lines.map(line => line.split(' ').map(s => +s));
+
+let sum = 0;
+
+for (let sequence of sequences) {
+  const levels = [ sequence ];
+  while (true) {
+    const differential = differentiate(sequence);
+    levels.push(differential);
+    if (!differential.find(n => n !== 0)) {
+      break;
+    }
+    sequence = differential;
+  }
+  levels.reverse();
+  let last = 0;
+  for (const level of levels.slice(1)) {
+    const next = level[level.length - 1] + last;
+    level.push(next);
+    last = next;
+  }
+  console.log(last);
+  sum += last;
+}
+
+console.log(sum);
+
+function differentiate(sequence) {
+  const d = [];
+  for (let i = 0; (i < sequence.length - 1); i++) {
+    d.push(sequence[i + 1] - sequence[i]);
+  }
+  return d;
+}
