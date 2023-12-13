9a3bff24bb6d65b35b577efdc7f3bd7f05dd5dea
boutell <tommybgoode@gmail.com>
2023-12-13 17:37:50 -0500

    memoization ftw

--- 12b.mjs
+++ 12b.mjs
@@ -12,6 +12,7 @@ console.log(puzzles.reduce((a, puzzle) => {
 }, 0));
 
 function solvePuzzle(puzzle) {
+  const memos = new Map();
   console.log('next:');
   console.log(puzzle.series.join(''));
   // You know exactly how many ones there are.
@@ -34,49 +35,60 @@ function solvePuzzle(puzzle) {
     for (let j = 0; (j < runs[0]); j++) {
       series[io++] = '#';
     }
-    solutions += solve(io, 1);
+    if (matchingSoFar(puzzle.series, series, io)) {
+      solutions += solve(io, 1);
+    }
   }
   return solutions;
   function solve(o, r) {
-    let s = 0;
-    for (let i = r; (i < runs.length); i++) {
-      s += runs[i];
+    const k = `${o}:${r}`;
+    if (memos.has(k)) {
+      return memos.get(k);
     }
-    const rl = runs.length - r;
-    if (o + s + rl > puzzle.series.length) {
-      return 0;
-    }
-    if (!rl) {
-      while (o < puzzle.series.length) {
-        series[o++] = '.';
+    const result = body();
+    memos.set(k, result);
+    return result;
+    function body() {
+      let s = 0;
+      for (let i = r; (i < runs.length); i++) {
+        s += runs[i];
       }
-      if (o !== puzzle.series.length) {
+      const rl = runs.length - r;
+      if (o + s + rl > puzzle.series.length) {
         return 0;
       }
-      if (!matchingSoFar(puzzle.series, series, o)) {
-        return 0;
+      if (!rl) {
+        while (o < puzzle.series.length) {
+          series[o++] = '.';
+        }
+        if (o !== puzzle.series.length) {
+          return 0;
+        }
+        if (!matchingSoFar(puzzle.series, series, o)) {
+          return 0;
+        }
+        if ((solutionCount % 1000000) === 0) {
+          console.log('PUZZLE: ' + puzzle.series.join(''));
+          console.log('SERIES: ' + series.join(''));
+        }
+        solutionCount++;
+        return 1;
       }
-      if ((solutionCount % 1000000) === 0) {
-        console.log('PUZZLE: ' + puzzle.series.join(''));
-        console.log('SERIES: ' + series.join(''));
+      let solutions = 0;
+      for (let i = 1; (i <= zeroes - count(series, '.', o) - (rl - 1)); i++) {
+        let io = o;
+        for (let j = 0; (j < i); j++) {
+          series[io++] = '.';
+        }
+        for (let j = 0; (j < runs[r]); j++) {
+          series[io++] = '#';
+        }
+        if (matchingSoFar(puzzle.series, series, io)) {
+          solutions += solve(io, r + 1);
+        }
       }
-      solutionCount++;
-      return 1;
+      return solutions;
     }
-    let solutions = 0;
-    for (let i = 1; (i <= zeroes - count(series, '.', o) - (rl - 1)); i++) {
-      let io = o;
-      for (let j = 0; (j < i); j++) {
-        series[io++] = '.';
-      }
-      for (let j = 0; (j < runs[r]); j++) {
-        series[io++] = '#';
-      }
-      if (matchingSoFar(puzzle.series, series, io)) {
-        solutions += solve(io, r + 1);
-      }
-    }
-    return solutions;
   }
 }
 
