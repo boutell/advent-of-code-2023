339b590cc90e6f82a19480fb4a268b91cf4bebeb
boutell <tommybgoode@gmail.com>
2023-12-20 18:31:01 -0500

    18b

--- 18b.mjs
+++ 18b.mjs
@@ -32,34 +32,35 @@ for (const move of moves) {
   edges.push(e);
 }
 
-const strokes = fill(edges);
-const grid = new SparseGrid();
-for (const stroke of strokes) {
-  for (let x = stroke.x1; (x <= stroke.x2); x++) {
-    grid.setValue(x, stroke.y, '#');    
-  }
-}
-grid.print();
-console.log(strokes.reduce((a, s) => a + (s.x2 - s.x1) + 1, 0));
+console.log(fill(edges, { area: true, progress: true }));
+// const strokes = fill(edges);
+// Grid would be too large to print
+// const grid = new SparseGrid();
+// for (const stroke of strokes) {
+//   for (let x = stroke.x1; (x <= stroke.x2); x++) {
+//     grid.setValue(x, stroke.y, '#');    
+//   }
+// }
+// grid.print();
 
-// 17a parser, for a tractable test case
-function parseMove(line) {
-  const [ , dir, dist, color ] = line.match(/^([UDLR]) (\d+) \((\S+)\)$/);
-  return {
-    dir,
-    dist: +dist,
-    color
-  };
-}
-
+// 18a parser, for a tractable test case
 // function parseMove(line) {
-//   let [ , dir, dist, color ] = line.match(/^([UDLR]) (\d+) \((\S+)\)$/);
-//   color = color.substring(1);
-//   color = parseInt(color, 16);
-//   dist = color >> 4;
-//   dir = [ 'R', 'D', 'L', 'U' ][color & 3];
+//   const [ , dir, dist, color ] = line.match(/^([UDLR]) (\d+) \((\S+)\)$/);
 //   return {
 //     dir,
-//     dist
+//     dist: +dist,
+//     color
 //   };
 // }
+
+function parseMove(line) {
+  let [ , dir, dist, color ] = line.match(/^([UDLR]) (\d+) \((\S+)\)$/);
+  color = color.substring(1);
+  color = parseInt(color, 16);
+  dist = color >> 4;
+  dir = [ 'R', 'D', 'L', 'U' ][color & 3];
+  return {
+    dir,
+    dist
+  };
+}
