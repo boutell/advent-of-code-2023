6fb6cff8a479c38e3ace38913154b8720d6eef97
boutell <tommybgoode@gmail.com>
2023-12-20 08:31:18 -0500

    wip 18b, testing with 18a parser for a simple test case, it's good on 18-ex but 18.txt is still drawing some errors

--- 18b.mjs
+++ 18b.mjs
@@ -1,4 +1,6 @@
 import { readFileSync } from 'fs';
+import fill from './lib/polygon-fill.mjs';
+import SparseGrid from './lib/sparse-grid.mjs';
 
 const input = readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n').filter(line => line.length > 0);
@@ -30,16 +32,34 @@ for (const move of moves) {
   edges.push(e);
 }
 
-console.log(edges);
+const strokes = fill(edges);
+const grid = new SparseGrid();
+for (const stroke of strokes) {
+  for (let x = stroke.x1; (x <= stroke.x2); x++) {
+    grid.setValue(x, stroke.y, '#');    
+  }
+}
+grid.print();
+console.log(strokes.reduce((a, s) => a + (s.x2 - s.x1) + 1, 0));
 
+// 17a parser, for a tractable test case
 function parseMove(line) {
-  let [ , dir, dist, color ] = line.match(/^([UDLR]) (\d+) \((\S+)\)$/);
-  color = color.substring(1);
-  color = parseInt(color, 16);
-  dist = color >> 4;
-  dir = [ 'R', 'D', 'L', 'U' ][color & 3];
+  const [ , dir, dist, color ] = line.match(/^([UDLR]) (\d+) \((\S+)\)$/);
   return {
     dir,
-    dist
+    dist: +dist,
+    color
   };
 }
+
+// function parseMove(line) {
+//   let [ , dir, dist, color ] = line.match(/^([UDLR]) (\d+) \((\S+)\)$/);
+//   color = color.substring(1);
+//   color = parseInt(color, 16);
+//   dist = color >> 4;
+//   dir = [ 'R', 'D', 'L', 'U' ][color & 3];
+//   return {
+//     dir,
+//     dist
+//   };
+// }
