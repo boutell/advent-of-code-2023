0fa2a5f339f655d1f075dfe6b444cc98fbe94095
boutell <tommybgoode@gmail.com>
2023-12-23 08:58:18 -0500

    this strategy for remembering what was sent is not good enough

--- 20b.mjs
+++ 20b.mjs
@@ -2,32 +2,40 @@ import { readFileSync } from 'fs';
 const input = readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n').filter(line => line.length > 0);
 const list = lines.map(parseModule);
-const modules = new Map();
 const button = {
   name: 'button',
   type: '',
-  dest: [ 'broadcaster' ],
-  layer: 0
+  dest: [ 'broadcaster' ]
+  // layer: 0
 };
 list.push(button);
-const output = {
-  name: 'output',
-  type: '',
-  dest: []
-};
-list.push(output);
 for (const m of list) {
-  modules.set(m.name, m);
-}
-for (const m of list) {
-  if (m.type === '&') {
-    const senders = list.filter(m2 => m2.dest.includes(m.name)).map(m2 => m2.name);
-    for (const sender of senders) {
-      m.memory.set(sender, false);
+  for (const d of m.dest) {
+    // he introduces rando modules sometimes
+    if (!list.find(m => m.name === d)) {
+      list.push({
+        name: d,
+        type: '',
+        dest: []
+      });
     }
   }
 }
+for (const m of list) {
+  m.senders = m.senders || [];
+}
+for (const m of list) {
+  m.sent = false;
+  m.dest = m.dest.map(name => list.find(m => m.name === name));
+  m.senders.push(m);
+}
+console.log(list);
 
+// LESSON: there's a ton of back propagation, the
+// quest for isolated "machines" would be difficult.
+//
+// ***
+//
 // I need to detect back propagation, e.g.
 // work out where the layers are so I can move
 // towards identifying islands I can simulate
@@ -44,34 +52,34 @@ for (const m of list) {
 // that is *less than* or *equal to* the current
 // layer number then we have back propagation ):
 
-findLayers(modules.get('button'));
-console.log('layer check complete');
-console.log(Math.max(...list.map(m => m.layer || 0)));
+// findLayers(modules.get('button'));
+// console.log('layer check complete');
+// console.log(Math.max(...list.map(m => m.layer || 0)));
 
-function findLayers(m) {
-  for (const name of m.dest) {
-    let m2 = modules.get(name);
-    if (!m2) {
-      // Because he likes to introduce unregistered
-      // modules at random I guess
-      m2 = {
-        name,
-        type: '',
-        dest: []
-      };
-      modules.set(name, m2);
-    }
-    if (m2.layer !== undefined) {
-      if (m2.layer < (m.layer + 1)) {
-        console.log(`Back propagation: ${m.name} ${m.layer} cycles back to ${m2.name} ${m2.layer}`);
-        continue;
-      }
-    } else {
-      m2.layer = m.layer + 1;
-    }
-    findLayers(m2);
-  }
-}
+// function findLayers(m) {
+//   for (const name of m.dest) {
+//     let m2 = modules.get(name);
+//     if (!m2) {
+//       // Because he likes to introduce unregistered
+//       // modules at random I guess
+//       m2 = {
+//         name,
+//         type: '',
+//         dest: []
+//       };
+//       modules.set(name, m2);
+//     }
+//     if (m2.layer !== undefined) {
+//       if (m2.layer < (m.layer + 1)) {
+//         console.log(`Back propagation: ${m.name} ${m.layer} cycles back to ${m2.name} ${m2.layer}`);
+//         continue;
+//       }
+//     } else {
+//       m2.layer = m.layer + 1;
+//     }
+//     findLayers(m2);
+//   }
+// }
 
 let sentHigh = 0;
 let sentLow = 0;
@@ -85,27 +93,17 @@ function solve() {
   while (true) {
     if (!(i % 1000000)) {
       console.log(i);
+      process.exit(0);
     }
     i++;
 
     pulses = [];
 
-    send(modules.get('button'), false);
+    send(button, false);
     
     while (pulses.length > 0) {
       const pulse = pulses.shift();
-      const sender = pulse.sender;
-      let m = modules.get(pulse.dest);
-      if (!m) {
-        // Because he likes to introduce unregistered
-        // modules at random I guess
-        m = {
-          name: pulse.dest,
-          type: '',
-          dest: []
-        };
-        modules.set(pulse.dest, m);
-      }
+      let m = pulse.dest;
       const value = pulse.value;
       if (value) {
         sentHigh++;
@@ -123,10 +121,9 @@ function solve() {
           send(m, m.state);
         }
       } else if (m.type === '&') {
-        m.memory.set(sender, value);
         let next = false;
-        for (const v of m.memory.values()) {
-          if (!v) {
+        for (const sender of m.senders) {
+          if (!sender.sent) {
             next = true;
           }
         }
@@ -137,11 +134,11 @@ function solve() {
 }
 
 function send(m, value) {
-  for (const name of m.dest) {
+  m.sent = value;
+  for (const d of m.dest) {
     pulses.push({
-      dest: name,
-      value,
-      sender: m.name
+      dest: d,
+      value
     });
   }
 }
